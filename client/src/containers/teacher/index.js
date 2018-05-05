dedd5b987762b2c20ae24bbe547c34b65333cacb
isthisnagee <isthisnagee@gmail.com>
2018-05-05 12:52:07 -0400

    teacher container + tabs + subjects

---
+++ client/src/containers/teacher/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+import { Analytics } from './Analytics';
+import { Subjects } from './Subjects';
+import './index.css';
+
+const Sidebar = () => (<p>hello</p>);
+
+const views = {
+  analytics: 'views/analytics',
+  quizEdit: 'views/create-game',
+};
+
+export class TeacherView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentView: views.quizEdit
+    };
+
+    this.handleSwitchToAnalytics = this.handleSwitchToAnalytics.bind(this);
+    this.handleSwitchToGame = this.handleSwitchToGame.bind(this);
+  }
+
+  handleSwitchToGame() {
+    this.setState({
+      currentView: views.quizEdit
+    });
+  }
+
+  handleSwitchToAnalytics() {
+    this.setState({
+      currentView: views.analytics
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Sidebar>
+        </Sidebar>
+        <Tabs className="">
+
+          <TabList className="flex list">
+            <Tab
+              onClick={this.handleSwitchToAnalytics} 
+              className="outline w-25 pa3 mr2">
+              Analytics
+            </Tab>
+            <Tab 
+              onClick={this.handleSwitchToGameView}
+              className="outline w-25 pa3 mr2">
+              Create Section
+            </Tab>
+          </TabList>
+
+          <TabPanel className="">
+            <Analytics />
+          </TabPanel>
+          <TabPanel className="">
+            <Subjects />
+          </TabPanel>
+        </Tabs>
+      </div>
+    );
+  }
+}
