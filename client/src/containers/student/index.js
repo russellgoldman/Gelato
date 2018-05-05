a6f1e824802b431ed2ca46b3f89766f2615b0263
Shomari Malcolm <shomarimalcolm@gmail.com>
2018-05-05 19:04:41 -0400

    changes made

--- client/src/containers/student/index.js
+++ client/src/containers/student/index.js
@@ -10,36 +10,74 @@ export class StudentView extends React.Component {
 
   constructor() {
     super();
-    this.state = {
-      currentAnswer: null,
+     this.state = {
       prevAnswer: null,
       attack: false,
-      correctAnswer: 4,
+      correctAnswer: false,
+      questions: [
+        {
+          id: 0,
+          question: "2x2",
+          answers: [
+            1,
+            4,
+            3,
+            5
+          ],
+          correctAnswer: 4,
+        },
+        {
+          id: 1,
+          question: "2x4",
+          answers: [
+            1,
+            2,
+            8,
+            5
+          ],
+          correctAnswer: 8,
+        }
+      ],
+      currentQuestionId: 0,
     }
   }
   
   handleAnswer = (answer) => {
-    const { prevAnswer, currentAnswer } = this.state;
+    const { questions, currentQuestionId, prevAnswer } = this.state;
+    const question = questions[currentQuestionId];
+    
     if (answer === prevAnswer) {
       this.setState({ attack: false });
+      return;
     };
-
     this.setState({
-      prevAnswer: currentAnswer,
-      currentAnswer: answer,
+      prevAnswer: answer,
+      correctAnswer: answer === question.correctAnswer,
       attack: true,
+    });
+  }
+  
+  stopAttack = () => {
+    // set new question vars based on next question
+    //stop animation and re render question UI
+    console.log("stopped!");
+    this.setState({
+      prevAnswer: null,
+      attack: false,
+      correctAnswer: false,
+       currentQuestionId: this.state.currentQuestionId+1
     });
   }
 
   render () {
     return (<div>
-      <div className='screen-container'> 
-        <StudentSideNav />
-        <div className='battle-screen-container'>
-          <BattleField onAnimationComplete={this.stopAttack} attack={this.state.attack} hit={this.state.currentAnswer === this.state.correctAnswer} />
-          <MCQuestion onAnswerSelected={this.handleAnswer}/>
+        <div className='screen-container'> 
+          <StudentSideNav />
+          <div className='battle-screen-container'>
+            <BattleField onAnimationComplete={this.stopAttack} attack={this.state.attack} hit={this.state.correctAnswer} />
+            <MCQuestion {...this.state.questions[this.state.currentQuestionId]} onAnswerSelected={this.handleAnswer}/>
+          </div>
         </div>
-      </div>
       {/*<div className='main-screen'>
         <StudentSideNav />
         <StudentClassSelect />
