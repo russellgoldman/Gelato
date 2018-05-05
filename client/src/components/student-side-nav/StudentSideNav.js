603a3f6152ce85be4c077dffe7de3c20e755d9b7
Shomari Malcolm <shomarimalcolm@gmail.com>
2018-05-05 16:03:14 -0400

    updated battle side nav

--- client/src/components/student-side-nav/StudentSideNav.js
+++ client/src/components/student-side-nav/StudentSideNav.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import coin from './coin.png';
-import robot from './robot.svg';
-import scroll from './scroll.png';
 import './student-side-nav.css';
 
 class  StudentSideNav extends Component {
@@ -9,18 +7,19 @@ class  StudentSideNav extends Component {
   render() {
     return (
       <div className='container'>
-        <h2>In Game Stats</h2>
         <div className='stat-item'>
-          <div>Profile Info</div>
-          <img src={robot} alt='robot'></img>
+          <div className='profile-box'></div>
+          <div className='profile-name'>Alice Liddel</div>
         </div>
-        <div className='stat-item'>
-          <div>Amount of Coins</div>
-          <img src={coin} alt='robot'></img>
-        </div>
-        <div className='stat-item'>
-          <div>Amount of Coins</div>
-          <img src={scroll} alt='robot'></img>
+        <div className='brief-stats'>
+          <div className='single-stat'>
+            <div className='circle'></div>
+            <div className='rectangle'></div>
+          </div>
+          <div className='single-stat'>
+            <div className='circle'></div>
+            <div className='rectangle'></div>
+          </div>
         </div>
       </div>
     );
