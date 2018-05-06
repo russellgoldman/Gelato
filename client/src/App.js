8a61dee71722c3ba3866ca68baa49cf5346dad04
Shomari Malcolm <shomarimalcolm@gmail.com>
2018-05-06 09:01:50 -0400

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import { TeacherView } from './containers/teacher';
+import { StudentView } from './containers/student';
+// import { LandingView } from './containers/landing';
 import { Titlebar } from './components/titlebar.js';
 import teacher from './images/teacher.png';
+import { ENGINE_METHOD_DIGESTS } from 'constants';
 
 const data = {
   classes: [
@@ -41,16 +44,30 @@ const data = {
     },
   ]
 };
+const views = {
+  LANDING: 'landing',
+  APP: {
+    STUDENT: 'student',
+    TEACHER: 'teacher',
+  }
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentView: views.LANDING,
       classes: data.classes,
       activeClassId: data.classes.findIndex(c => c.active)
     };
     this.removeSubjectFromActiveCourse = this.removeSubjectFromActiveCourse.bind(this);
     this.addSubjectToActiveCourse = this.addSubjectToActiveCourse.bind(this);
+  }
+
+  updateView = (view) => {
+    this.setState({
+      currentView: view,
+    })
   }
 
   addSubjectToActiveCourse(subjectName) {
@@ -78,22 +95,49 @@ class App extends Component {
     });
   }
 
+  renderApp() {
+    if (this.state.currentView === views.LANDING) return;
+    const studentOrTeacher = () => {
+      switch (this.state.currentView) {
+        case views.APP.STUDENT: return <StudentView />
+        case views.APP.TEACHER: return (
+        <TeacherView
+          currentClass={this.state.classes[this.state.activeClassId]}
+          classes={this.state.classes}
+          className="vh-90"
+          fns={{
+            addSubject: this.addSubjectToActiveCourse,
+            removeSubject: this.removeSubjectFromActiveCourse,
+          }}
+        />)
+      }
+    }
+    return (
+      <div>
+      <Titlebar className="vh-10" imgSrc={teacher} imgAlt="Teacher avatar"></Titlebar>
+      {studentOrTeacher()}
+      </div>
+    )
+  }
+
+  renderLanding() {
+    if (this.state.currentView !== views.LANDING) return;
+    return (
+      <div>
+        <button onClick={() => this.updateView(views.APP.STUDENT)}>STUDENT</button>
+        <button onClick={() => this.updateView(views.APP.TEACHER)}>TEACHER</button>
+      </div>
+    )
+  }
   render() {
     const { currentView } = this.state;
     const show = (view, component) => currentView === view && component;
 
     return (
       <div className="vh-100">
-        <Titlebar className="vh-10" imgSrc={teacher} imgAlt="Teacher avatar"></Titlebar>
-        <TeacherView 
-          currentClass={this.state.classes[this.state.activeClassId]}
-          classes={this.state.classes}
-          className="vh-90" 
-          fns={{
-            addSubject: this.addSubjectToActiveCourse,
-            removeSubject: this.removeSubjectFromActiveCourse,
-          }}
-        />
+        {this.renderLanding()}
+        {this.renderApp()}
+
       </div>
     );
   }
