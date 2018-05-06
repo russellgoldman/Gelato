a738088df5fcfcfaf469b07d61e0d2b58a92f184
Shomari Malcolm <shomarimalcolm@gmail.com>
2018-05-06 00:56:01 -0400

    side nav edits

--- client/src/components/student-side-nav/StudentSideNav.js
+++ client/src/components/student-side-nav/StudentSideNav.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './student-side-nav.css';
+import coins from './coin.png';
+import scroll from './scroll.png';
 
 class  StudentSideNav extends Component {
 
@@ -12,12 +14,21 @@ class  StudentSideNav extends Component {
         </div>
         <div className='brief-stats'>
           <div className='single-stat'>
-            <div className='circle'></div>
-            <div className='rectangle'></div>
+            <div className='circle'>
+              <img className='coin-circle' src={coins}></img>
+            </div>
+            <div className='rectangle'><h3>5000c</h3></div>
           </div>
           <div className='single-stat'>
-            <div className='circle'></div>
-            <div className='rectangle'></div>
+            <div className='circle'>
+              <img className='scroll-circle' src={scroll}></img>
+            </div>
+            <div className='rectangle'><h5>5 Quests Complete</h5></div>
+          </div>
+          <div className='extra-info'>
+            <h3 className='level-stat'>Curent Level: 1</h3>
+            <h3 className='map-stat'>Map: Division</h3>
+            <h3 className='dungeon-stat'>Dungeon 1</h3>
           </div>
         </div>
       </div>
